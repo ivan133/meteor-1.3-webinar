8aeb19d85974850c7d45a6fba507c3a3931c1a31
Ivan Rozhcov <ivan133133@gmail.com>
2016-06-12 23:38:27 +0300

    шаг 1.8 настроено разрешение на insert

--- imports/api/messages.js
+++ imports/api/messages.js
@@ -1,3 +1,12 @@
 import { Mongo } from 'meteor/mongo';
 
 export const Messages = new Mongo.Collection("messages");
+
+Messages.allow({
+    insert(userid, message){
+        if (message.user_id && message.user_id != userid) {
+            throw new Meteor.Error('incorrect user');
+        }
+        return true
+    }
+});
