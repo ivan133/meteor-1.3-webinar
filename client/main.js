ab92fb9b812b2de929aec2cd4bb173b6721446a4
Ivan Rozhcov <ivan133133@gmail.com>
2016-06-12 14:12:10 +0300

    шаг 1.5 приватные посты, autopublish включен

--- client/main.js
+++ client/main.js
@@ -7,17 +7,23 @@ import './main.html';
 Template.messages.helpers(
     {
         messagesList() {
-            return Messages.find({},
-                {sort: {time: -1}});
+            if (Meteor.userId())
+                return Messages.find({$or: [{is_private: {$ne: true}}, {is_private: true, user_id: Meteor.userId()}]},
+                    {sort: {time: -1}});
+            else
+                return Messages.find({is_private: {$ne: true}},
+                    {sort: {time: -1}});
         }
     });
 Template.messages.events({
     "click #add-message" (e) {
         var mes = prompt("Please enter message", "");
+        var isPrivate = $('#is-private').is(":checked");
         Messages.insert({
             message: mes, time: Date.now(),
             user_id: Meteor.userId(),
-            username: Meteor.user().emails[0].address
+            username: Meteor.user().emails[0].address,
+            is_private: isPrivate
         });
     }
 });
